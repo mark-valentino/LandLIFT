1122a84cb2b6c59658cccd8d27583063c47ebf14
Mark Valentino Jr <mvalentino@landintelligence.net>
2023-06-27 14:22:03 -0400

    added Amplify studio library

--- screens/Details.tsx
+++ screens/Details.tsx
@@ -4,17 +4,7 @@ import {Pressable, View, Text} from 'react-native';
 export function Details({navigation}: {navigation: any}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Welcome to our Details Screen</Text>
-      <Pressable
-        onPress={() => navigation.navigate('Conference')}
-        style={{
-          backgroundColor: 'plum',
-          padding: 10,
-          marginBottom: 10,
-          marginTop: 10,
-        }}>
-        <Text>Conference</Text>
-      </Pressable>
+      <Text>Page 1</Text>
       <Pressable
         onPress={() => navigation.navigate('Home')}
         style={{backgroundColor: 'plum', padding: 10}}>
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -4,17 +4,7 @@ import {Pressable, View, Text} from 'react-native';
 export function Home({navigation}: {navigation: any}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Welcome to our Home Screen</Text>
-      <Pressable
-        onPress={() => navigation.navigate('Conference')}
-        style={{
-          backgroundColor: 'plum',
-          padding: 10,
-          marginBottom: 10,
-          marginTop: 10,
-        }}>
-        <Text>Conference</Text>
-      </Pressable>
+      <Text>Page 2</Text>
       <Pressable
         onPress={() => navigation.navigate('Details')}
         style={{backgroundColor: 'plum', padding: 10}}>
