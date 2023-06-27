0e59b09b8acc3ff7f72ab00cde7f2887d2cb5ffe
Mark Valentino Jr <mvalentino@landintelligence.net>
2023-06-27 18:23:27 -0400

    started making login page

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
-import {Pressable, View, Text} from 'react-native';
+import {Pressable, Text, TextInput, View} from 'react-native';
 
 export function Home({navigation}: {navigation: any}) {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#fff',
+      }}>
+      <TextInput style={{backgroundColor: 'red', width: '100%'}} />
+      <TextInput style={{backgroundColor: 'red', width: '100%'}} />
       <Text>Page 2</Text>
       <Pressable
         onPress={() => navigation.navigate('Details')}
-        style={{backgroundColor: 'plum', padding: 10}}>
+        style={{backgroundColor: 'plum', padding: 10, width: '100%'}}>
         <Text>Story</Text>
       </Pressable>
     </View>
